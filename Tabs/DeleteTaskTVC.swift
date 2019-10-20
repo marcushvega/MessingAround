24ce44cb2901fae5fe3e0955efc2ea6979270c29
Marcus Vega <marcus.h.vega@gmail.com>
2019-10-20 11:41:37 -0400

    Added VISUAL functionality to let a user swipe a row to delete that item. I still need to implement code to delete the item from some entity record because an error is currently thrown when user swipes a row to delete that item.

--- Tabs/DeleteTaskTVC.swift
+++ Tabs/DeleteTaskTVC.swift
@@ -6,14 +6,29 @@ import CoreData
 class DeleteTaskTVC: UITableViewController {
 
     var taskList: [NSManagedObject] = []
+    @IBOutlet weak var deleteButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         pullSavedData()
+        setupDeleteButton()
+        setupTableView()
+//        self.clearsSelectionOnViewWillAppear = false
+    }
+    
+    // MARK: - Setup UI Elements
+    func setupDeleteButton() {
+        deleteButton.setTitle("Delete", for: .normal)
+        deleteButton.layer.borderWidth = 2.5
+        deleteButton.layer.cornerRadius = 10
+        deleteButton.backgroundColor = UIColor.init(red: 214/255, green: 26/255, blue: 60/255, alpha: 0.6)
+        deleteButton.setTitleColor(UIColor.white, for: .normal)
+//        deleteButton.setTitleColor(UIColor.red, for: .selected)
+    }
+    
+    func setupTableView() {
         tableView.backgroundColor = UIColor(cgColor: CGColor.init(srgbRed: 160/255, green: 165/255, blue: 247/255, alpha: 1))
         tableView.rowHeight = 50
-
-//        self.clearsSelectionOnViewWillAppear = false
     }
 
     // MARK: - Table view data source
@@ -27,7 +42,8 @@ class DeleteTaskTVC: UITableViewController {
         
         cell.textLabel?.text = "\(task.value(forKeyPath: "title") as! String)"
         cell.textLabel?.adjustsFontSizeToFitWidth = true
-        cell.backgroundColor = UIColor(cgColor: CGColor.init(srgbRed: 233/255, green: 235/255, blue: 152/255, alpha: 0.5))
+        cell.textLabel?.textColor = UIColor(cgColor: CGColor.init(srgbRed: 247/255, green: 247/255, blue: 247/255, alpha: 1))
+        cell.backgroundColor = UIColor(cgColor: CGColor.init(srgbRed: 160/255, green: 165/255, blue: 247/255, alpha: 1))
         cell.accessoryType = .none
         
         return cell
@@ -39,60 +55,57 @@ class DeleteTaskTVC: UITableViewController {
         if let cell = tableView.cellForRow(at: indexPath as IndexPath) {
             if (cell.accessoryType == UITableViewCell.AccessoryType.none) {
                 cell.accessoryType = .checkmark
+                cell.backgroundColor = UIColor(cgColor: CGColor.init(srgbRed: 200/255, green: 161/255, blue: 247/255, alpha: 0.8))
             }
             else {
                 cell.accessoryType = UITableViewCell.AccessoryType.none
+                cell.backgroundColor = UIColor(cgColor: CGColor.init(srgbRed: 160/255, green: 165/255, blue: 247/255, alpha: 1))
             }
         }
     }
     
-    // MARK: - Pulling data for CoreData methods
-        func pullSavedData() {
-            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-                return
-            }
-
-            let managedContext = appDelegate.persistentContainer.viewContext
-            let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "TaskItem")
+    // MARK: - Code for Deleting a Section from the table
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
     
-            do {
-                taskList = try managedContext.fetch(fetchRequest)
-            } catch let error as NSError {
-                print("Could not fetch. \(error), \(error.userInfo)")
-            }
-    
+    // DO I WANT TO USE .cellForRowAt IF I AM TRYING TO DELETE A SECTION?
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath as IndexPath) else {return}
+        if (editingStyle == UITableViewCell.EditingStyle.delete) {
+            print("We now attempt to delete a cell with its editingStyle.delete button")
+            deleteSavedData(taskName: cell.textLabel?.text as! String)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
         }
+    }
         
-    
-    //    override func viewDidAppear(_ animated: Bool) {
-    //        pullSavedData()
-    //        print("here is viewDidAppear")
-    //        print(pullSavedData())
-    //    }
-        
-    
-        
-    //    @IBAction func deleteTask(taskName: String, taskTime: Int) {
-    //        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-    //            return
-    //        }
-    //
-    //        let managedContext = appDelegate.persistentContainer.viewContext
-    //        let entity = NSEntityDescription.entity(forEntityName: "TaskItem", in: managedContext)!
-    //        let taskItem = NSManagedObject(entity: entity, insertInto: managedContext)
-    //
-    //        taskItem.setValue(taskName, forKey: "title")
-    //        taskItem.setValue(taskTime, forKey: "time")
-    //
-    //        // delete said task
-    //        managedContext.delete(taskItem)
-    //
-    ////        taskItems.remove(at: )
-    //
-    //    }
+    // MARK: - Using data for CoreData methods
+    func pullSavedData() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
 
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "TaskItem")
+
+        do {
+            taskList = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
     
-    
+    func deleteSavedData(taskName: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "TaskItem", in: managedContext)!
+        let taskItem = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        taskItem.setValue(taskName, forKey: "title")
+        
+        // delete said task
+        managedContext.delete(taskItem)
+    }
 }
 
   
